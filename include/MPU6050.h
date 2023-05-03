5eb603e8e9c6593069b6e48679f95d6c5902f7a6
eccentricOrange <eccentric.orange2@gmail.com>
2023-05-03 23:41:30 +0530

    successfully read accelerometer

---
+++ include/MPU6050.h
@@ -0,0 +1,106 @@
+/* MPU6050.h - Code for using the MPU6050 accelerometer and gyroscope.
+Much of this was sourced from https://wolles-elektronikkiste.de/en/mpu6050-accelerometer-and-gyroscope
+*/
+
+#if !defined(MPU6060_h)
+#define MPU6060_h
+
+#include <Arduino.h>
+#include <Wire.h>
+
+class MPU6050 {
+    private:
+        TwoWire* _wireInterface;
+        int _address;
+        const int16_t _ACCELEROMETER_MAXIMUMS[3] = {6350, 32767, 14450};
+        const int16_t _ACCELEROMETER_MINIMUMS[3] = {-26350, -32768, -18400};
+
+    public:
+        MPU6050(TwoWire* wireInterface, int address);
+        void initialize();
+        void readValues(
+            int16_t accelerometer[3],
+            int16_t gyroscope[3],
+            int16_t* temperature
+        );
+        void printAccelerometerPeaks(Stream* serialInterface, int16_t accelerometerReadings[3]);
+        float* correct(int16_t accelerometerReadings[3]);
+};  // class MPU6050
+
+MPU6050::MPU6050(TwoWire* wireInterface, int address) {
+    _wireInterface = wireInterface;
+    _address = address;
+}  // MPU6050::MPU6050
+
+void MPU6050::initialize() {
+    (*_wireInterface).beginTransmission(_address);
+    (*_wireInterface).write(0x6B);  // PWR_MGMT_1 register
+    (*_wireInterface).write(0);     // wake up!
+    (*_wireInterface).endTransmission(true);
+}  // MPU6050::initialize
+
+void MPU6050::readValues(int16_t accelerometer[3], int16_t gyroscope[3], int16_t* temperature) {
+    (*_wireInterface).beginTransmission(_address);
+    (*_wireInterface).write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    (*_wireInterface).endTransmission(false);  // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+    (*_wireInterface).requestFrom(_address, 14, true);  // request a total of 7*2 = 14 registers
+
+    // "(*_wireInterface).read()<<8 | (*_wireInterface).read();" means two registers are read and
+    // stored in the same int16_t variable
+
+    // Read the registers:
+    // X: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+    // Y: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+    // Z: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+    for (int i = 0; i < 3; i++) {
+        accelerometer[i] = (*_wireInterface).read() << 8 | (*_wireInterface).read();
+    }
+
+    // Temperature: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+    *temperature = (*_wireInterface).read() << 8 | (*_wireInterface).read();
+
+    // X: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+    // Y: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+    // Z: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+    for (int i = 0; i < 3; i++) {
+        gyroscope[i] = (*_wireInterface).read() << 8 | (*_wireInterface).read();
+    }
+}  // MPU6050::readValues
+
+void MPU6050::printAccelerometerPeaks(Stream* serialInterface, int16_t accelerometerReadings[3]) {
+    static int16_t accelerometerMaximums[3];
+    static int16_t accelerometerMinimums[3];
+
+    if (accelerometerMaximums[0] == 0) {
+        for (size_t i = 0; i < 3; i++) {
+            accelerometerMaximums[i] = accelerometerReadings[i];
+            accelerometerMinimums[i] = accelerometerReadings[i];
+        }
+    }
+
+    for (size_t i = 0; i < 3; i++) {
+        if (accelerometerReadings[i] > accelerometerMaximums[i]) {
+            accelerometerMaximums[i] = accelerometerReadings[i];
+        }
+
+        if (accelerometerReadings[i] < accelerometerMinimums[i]) {
+            accelerometerMinimums[i] = accelerometerReadings[i];
+        }
+    }
+
+    (*serialInterface).printf("X(%d, %d, %d) | ", accelerometerReadings[0], accelerometerMaximums[0], accelerometerMinimums[0]);
+    (*serialInterface).printf("Y(%d, %d, %d) | ", accelerometerReadings[1], accelerometerMaximums[1], accelerometerMinimums[1]);
+    (*serialInterface).printf("Z(%d, %d, %d)\n", accelerometerReadings[2], accelerometerMaximums[2], accelerometerMinimums[2]);
+}  // MPU6050::printAccelerometerPeaks
+
+float* MPU6050::correct(int16_t accelerometerReadings[3]) {
+    static float correctedAccelerometerReadings[3];
+
+    for (size_t i = 0; i < 3; i++) {
+        correctedAccelerometerReadings[i] = (float)(accelerometerReadings[i] - _ACCELEROMETER_MINIMUMS[i]) / (_ACCELEROMETER_MAXIMUMS[i] - _ACCELEROMETER_MINIMUMS[i]) * 2 - 1;
+    }
+
+    return correctedAccelerometerReadings;
+}  // MPU6050::correct
+
+#endif // MPU6060_h
--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,29 @@
 #include <Arduino.h>
-
 #include "Wire.h"
+#include "MPU6050.h"
 
-const int IMU_ADDRESS = 0x68;  // Alternatively set AD0 to HIGH  --> Address = 0x69
+MPU6050 mpu6050(&Wire, 0x68);
 
-int accelerometerReadings[3];
-int accelerometerMaximums[3];
-int accelerometerMinimums[3];
-
-int gyroscopeReadings[3];
-
-int temperatureReading;
-
-void initializeIMU(TwoWire* wireInterface, int address) {
-    (*wireInterface).beginTransmission(address);
-    (*wireInterface).write(0x6B);  // PWR_MGMT_1 register
-    (*wireInterface).write(0);     // wake up!
-    (*wireInterface).endTransmission(true);
-}
-
-void readIMUValues(
-    TwoWire* wireInterface,
-    int address,
-    int accelerometer[3],
-    int gyroscope[3],
-    int* temperature
-) {
-    (*wireInterface).beginTransmission(IMU_ADDRESS);
-    (*wireInterface).write(0x3B);             // starting with register 0x3B (ACCEL_XOUT_H)
-    (*wireInterface).endTransmission(false);  // the parameter indicates that the Arduino will send a restart.
-                                  // As a result, the connection is kept active.
-    (*wireInterface).requestFrom(IMU_ADDRESS, 14, true);  // request a total of 7*2=14 registers
-
-    // "(*wireInterface).read()<<8 | (*wireInterface).read();" means two registers are read and stored in the same int16_t
-    // variable
-    for (int i = 0; i < 3; i++) {
-        accelerometer[i] = (*wireInterface).read() << 8 | (*wireInterface).read();
-    }
-
-    *temperature = (*wireInterface).read() << 8 | (*wireInterface).read();
-
-    for (int i = 0; i < 3; i++) {
-        gyroscope[i] = (*wireInterface).read() << 8 | (*wireInterface).read();
-    }
-}
-
-void getPeaks(int* maximum, int* minimum, int reading) {
-    if (reading > *maximum) {
-        *maximum = reading;
-    }
-
-    if (reading < *minimum) {
-        *minimum = reading;
-    }
-}
+int16_t accelerometer[3], gyroscope[3];
+float* correctedAccelerometer;
+int16_t temperature;
 
 void setup() {
     Serial.begin(115200);
-
     Wire.begin();
-    initializeIMU(&Wire, IMU_ADDRESS);
-
-    readIMUValues(
-        &Wire,
-        IMU_ADDRESS,
-        accelerometerReadings,
-        gyroscopeReadings,
-        &temperatureReading
-    );
-
-    for (int i = 0; i < 3; i++) {
-        accelerometerMaximums[i] = accelerometerReadings[i];
-        accelerometerMinimums[i] = accelerometerReadings[i];
-    }
+    mpu6050.initialize();
 }
 
 void loop() {
-    readIMUValues(
-        &Wire,
-        IMU_ADDRESS,
-        accelerometerReadings,
-        gyroscopeReadings,
-        &temperatureReading
+    mpu6050.readValues(accelerometer, gyroscope, &temperature);
+    correctedAccelerometer = mpu6050.correct(accelerometer);
+
+    Serial.printf(
+        "Accelerometer: %f, %f, %f\n",
+        correctedAccelerometer[0],
+        correctedAccelerometer[1],
+        correctedAccelerometer[2]
     );
-
-    for (int i = 0; i < 3; i++) {
-        getPeaks(
-            &accelerometerMaximums[i],
-            &accelerometerMinimums[i],
-            accelerometerReadings[i]
-        );
-    }
-
-    Serial.printf("X(%d, %d, %d)| ", accelerometerReadings[0], accelerometerMaximums[0], accelerometerMinimums[0]);
-    Serial.printf("Y(%d, %d, %d)| ", accelerometerReadings[1], accelerometerMaximums[1], accelerometerMinimums[1]);
-    Serial.printf("Z(%d, %d, %d)\n", accelerometerReadings[2], accelerometerMaximums[2], accelerometerMinimums[2]);
 
     delay(100);
 }
-
-// After rotating the IMU around a lot, the following data was obtained.
-// X(59012, 65472, 164)| Y(32767, 32767, 32767)| Z(3508, 65532, 32)
